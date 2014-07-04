5b705005eb3e4e9a91d5b55b7f822164ed051c31
Anuj Pandey <a@e10.in>
2014-07-04 21:37:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var util = require('util'),
 var Que=function(args){
 	EventEmitter.call(this);
 	var self=this;
-	var sending=0;
+	var sending=0,vz = new VidZapper(args.vidzapper);
 	var _push=function(topic,obj,next){
 		self.vz.post('que/new',{topic:topic,request:JSON.stringify(obj),priority:1},next);
 	}
@@ -13,12 +13,24 @@ var Que=function(args){
 		self.vz.post('que/plenty',{topic:topic,requests:requests,priority:1},next)
 	};
 	Que.prototype.poll = function(topic) {
+		var _next=function(){
+			self.poll(topic);
+		}
 		self.vz.post('que/next',{topic:topic},function(d){
-			self.emit('found',d);
+			if(!d){
+                setTimeout(_next,1000*60);
+                return;
+            }else{
+				self.emit('found',d,_next);
+			}
 		})
 	};
 	Que.prototype.push = _push;
-	Que.prototype.vz = new VidZapper(args.vidzapper);
+	Que.prototype.vz = vz;
+	Que.prototype.storage=function(id,cb){vz.api('util/storage',{id:id},cb);}
+	Que.prototype.profiles=function(info,cb){vz.api('util/profile',info, cb);}
+	Que.prototype.master=function(info,cb){vz.api('util/master',info, cb);}
+	Que.prototype.vze=function(model,cb){vz.api('util/vze',model,cb);}
 }
 util.inherits(Que, EventEmitter);
 exports = module.exports = function(args) {
