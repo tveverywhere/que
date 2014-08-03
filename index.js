d5ace3e9b40f1379f3802ff3465a9d101eb0ac6e
qanuj <a@e10.in>
2014-08-03 19:21:48 +0530

    v0.0.12

--- index.js
+++ index.js
@@ -6,28 +6,26 @@ var Que=function(args){
 	EventEmitter.call(this);
 	var self=this;
 	var sending=0,vz = new VidZapper(args.vidzapper);
-	var _push=function(topic,obj,next){
-		vz.post('que/new',{topic:topic,request:JSON.stringify(obj),priority:1},next);
-	}
+
 	Que.prototype.plentyQue = function(topic,requests,next) {
-		vz.post('que/plenty',{topic:topic,requests:requests,priority:1},next)
+		vz.post2('que/plenty',{topic:topic,requests:requests,priority:1},next)
 	};
 
 	Que.prototype.registerMachine = function(obj) {
-		vz.post('machine/register',obj,function(d){
+		vz.post2('machine/register',obj,function(d){
 			self.emit('register',{who:'machine',entity:d});
 		})
 	};
 
 	Que.prototype.registerAgent = function(obj) {
-		vz.post('agent/register',obj,function(d){
+		vz.post2('agent/register',obj,function(d){
 			self.emit('register',{who:'agent',entity:d});
 		})
 	};
 
 	Que.prototype.poll = function(topic) {
 		var _next=function(topic){self.poll(topic);}
-		vz.post('que/next',{topic:topic},function(d){
+		vz.post2('que/next',{topic:topic},function(d){
 			if(!d || d.error ){
 				console.log('will try again in 60 seconds');
                 setTimeout(_next,1000*60,topic);
@@ -38,12 +36,13 @@ var Que=function(args){
 		})
 	};
 
-	Que.prototype.push = _push;
-	Que.prototype.vz = vz;
-	Que.prototype.storage=function(id,cb){vz.api('util/storage',{id:id},cb);}
+	Que.prototype.push = function(topic,obj,next){
+		vz.post2('que/new',{topic:topic,request:JSON.stringify(obj),priority:1},next);
+	};
+	Que.prototype.storage=function(id,cb){vz.api2('util/storage',{id:id},cb);}
 	Que.prototype.profiles=function(id,cb){vz.get2('util/profile/'+id, cb);}
-	Que.prototype.master=function(info,cb){vz.api('util/master',info, cb);}
-	Que.prototype.vze=function(model,cb){vz.api('util/vze',model,cb);}
+	Que.prototype.master=function(info,cb){vz.api2('util/master',info, cb);}
+	Que.prototype.vze=function(model,cb){vz.api2('util/vze',model,cb);}
 }
 util.inherits(Que, EventEmitter);
 exports = module.exports = function(args) {
